6c2b6b2a5a1694d71ce5aa944f715d7c246adc7a
Richard Neumann <r.neumann@homeinfo.de>
2022-01-17 12:57:20 +0100

    Migrate to setuptools
    While at it, add dependencies

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
 #! /usr/bin/env python3
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='notificationlib',
-    version='latest',
+    use_scm_version={
+        "local_scheme": "node-and-timestamp"
+    },
+    setup_requires=['setuptools_scm'],
+    install_requires=[
+        'configlib',
+        'emaillib',
+        'flask',
+        'his',
+        'mdb',
+        'peewee',
+        'wsgilib'
+    ],
     author='HOMEINFO - Digitale Informationssysteme GmbH',
     author_email='<info at homeinfo dot de>',
     maintainer='Richard Neumann',
